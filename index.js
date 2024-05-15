57c78a831f1fca64119738d3f36e736cd1aaf54f
diasp10 <gustavo.creep@gmail.com>
2024-05-15 10:49:32 +0000

    Endpoint /oi adicionado + Criacao do script 'start'

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+//Endpoint /oi -> Olá Mundo
+app.get('/oi', function (req,res) {
+  res.send ('Olá, Mundo!')
+})
+
 app.listen(3000)
+console.log('Aplicação rodando em http://localhost:3000/oi')
